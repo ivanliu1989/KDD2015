2c8f461a0b2676d11a733986c905cdb118f59740
ivanliu1989 <ivan.liuyanfeng@gmail.com>
2015-05-01 16:54:52 +1000

    init

--- 0_init.R
+++ 0_init.R
@@ -1,2 +1,15 @@
 setwd('Google Drive/KDD2015')
 rm(list = ls()); gc()
+require(data.table)
+
+submission <- fread('Data/sampleSubmission.csv')
+head(submission); dim(submission)
+
+object <- fread('Data/object.csv')
+head(object); dim(object)
+table(object$category)
+
+train_enrol <- fread('Data/train/enrollment_train.csv')
+str(train_enrol); head(train_enrol)
+train_log <- fread('Data/train/log_train.csv')
+str(train_log); head(train_log)
