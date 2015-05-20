4ed8b1ce1064d32b8ac16e212dfeba5c57f334e6
ivanliu1989 <ivan.liuyanfeng@gmail.com>
2015-05-20 10:30:59 +1000

    feature construction

--- 3_exploration.R
+++ 3_exploration.R
@@ -2,14 +2,33 @@ setwd('Google Drive/KDD2015')
 rm(list = ls()); gc()
 require(data.table);require(caret)
 
-train_log <- fread('data/train/log_extended_train.csv', data.table=F)
-test_log <- fread('data/test/log_extended_test.csv', data.table=F)
+train_log <- fread('data/train/log_train.csv', data.table=F)
+test_log <- fread('data/test/log_test.csv', data.table=F)
 train <- fread('data/train/enrollment_train.csv', data.table=F)
 test <- fread('data/test/enrollment_test.csv', data.table=F)
 target <- fread('data/train/truth_train.csv', data.table=F)
 colnames(target) <- c('enrollment_id','dropout')
 
-generateDistribution <- function(x,name,gap=0.2) {
+generateDistribution <- function(x) {
+    name='hourDist'
+    gap=0.2
+    x_wo_na <- x[!is.na(x)]
+    qdist <- seq(gap,1, by = gap)
+    if (length(x_wo_na)<(2*length(qdist))) {
+        dist <- quantile(x_wo_na, qdist)
+    } else {
+        x_wo_peaks <- x_wo_na[abs(x_wo_na-mean(x_wo_na,na.rm = TRUE)) 
+                              < 5*sd(x_wo_na,na.rm = TRUE)]
+        dist <- quantile(x_wo_peaks, qdist)
+    }
+    names(dist) = paste(name,names(dist),sep='_')
+    names(dist) = gsub("%", "_pct", names(dist))
+    return(dist)
+}
+generateDistributionDiff <- function(x) {
+    x <- as.numeric(diff(x))
+    name='freqDist'
+    gap=0.2
     x_wo_na <- x[!is.na(x)]
     qdist <- seq(gap,1, by = gap)
     if (length(x_wo_na)<(2*length(qdist))) {
@@ -25,6 +44,8 @@ generateDistribution <- function(x,name,gap=0.2) {
 }
 
 featureEngineering <- function(x, x_log) {
+    x_log$POSIX <- strptime(x_log$time, "%Y-%m-%dT%H:%M:%S")
+    
     # Source - serverCount/browserCount
     newFeat <- as.matrix(aggregate(x_log$source,list(x_log$enrollment_id),FUN=table))
     colnames(newFeat) <- c('enrollment_id','browserCount','serverCount')
@@ -36,38 +57,45 @@ featureEngineering <- function(x, x_log) {
                            'page_closeCount','problemCount','videoCount','wikiCount')
     x <- merge(x, newFeat, sort=F, all=T)
     
-    # Time - Variance
-    newFeat <- as.matrix(aggregate(x_log$time,list(x_log$enrollment_id),FUN=var))
-    colnames(newFeat) <- c('enrollment_id','timeVar')
-    x <- merge(x, newFeat, sort=F, all=T)
-    
     # Time - Standard Deviation
-    newFeat <- as.matrix(aggregate(x_log$time,list(x_log$enrollment_id),FUN=sd))
+    newFeat <- as.matrix(aggregate(x_log$POSIX,list(x_log$enrollment_id),FUN=sd))
     colnames(newFeat) <- c('enrollment_id','timeSd')
     x <- merge(x, newFeat, sort=F, all=T)
     
     # Time - Duration
-    newFeat <- as.matrix(aggregate(x_log$time,list(x_log$enrollment_id),FUN=function(x) max(x)-min(x)))
+    newFeat <- as.matrix(aggregate(x_log$POSIX,list(x_log$enrollment_id),
+                                   FUN=function(x) max(x)-min(x)))
     colnames(newFeat) <- c('enrollment_id','timeDuration')
     x <- merge(x, newFeat, sort=F, all=T)
     
-    # Time - hourDistribution
-    x_log$hour <- as.numeric(format(x_log$time, "%H")) + as.numeric(format(x_log$time, "%M"))/60
+    # Time - hourDistribution/hourMean
+    x_log$hour <- as.numeric(format(x_log$POSIX, "%H")) + as.numeric(format(x_log$POSIX, "%M"))/60
+    newfeat <- as.matrix(aggregate(x_log$hour,list(x_log$enrollment_id),FUN=generateDistribution))
+    colnames(newfeat) <- c('enrollment_id','hourDist_20','hourDist_40','hourDist_60','hourDist_80','hourDist_100')
+    x <- merge(x, newFeat, sort=F, all=T)
+    
     newfeat <- as.matrix(aggregate(x_log$hour,list(x_log$enrollment_id),FUN=mean))
     colnames(newfeat) <- c('enrollment_id','hourMean')
     x <- merge(x, newFeat, sort=F, all=T)
     
     # Time - skewnessTime
-    newFeat <- as.matrix(aggregate(as.numeric(x_log$time),list(x_log$enrollment_id),FUN=skewness))
+    newFeat <- as.matrix(aggregate(as.numeric(x_log$POSIX),list(x_log$enrollment_id),FUN=skewness))
     colnames(newFeat) <- c('enrollment_id','timeSkew')
     x <- merge(x, newFeat, sort=F, all=T)
     
     # Time - kurtosisTime
-    newFeat <- as.matrix(aggregate(as.numeric(x_log$time),list(x_log$enrollment_id),FUN=kurtosis))
+    newFeat <- as.matrix(aggregate(as.numeric(x_log$POSIX),list(x_log$enrollment_id),FUN=kurtosis))
     colnames(newFeat) <- c('enrollment_id','timeKurt')
     x <- merge(x, newFeat, sort=F, all=T)
     
-    # Time - Frequency
+    # Time - FrequencyDistribution/FreqMean
+    newFeat <- as.matrix(aggregate(x_log$POSIX,list(x_log$enrollment_id),FUN=function(x) mean(diff(x))))
+    colnames(newFeat) <- c('enrollment_id','freqMean')
+    x <- merge(x, newFeat, sort=F, all=T)
+    
+    newFeat <- as.matrix(aggregate(x_log$POSIX,list(x_log$enrollment_id),FUN=generateDistributionDiff))
+    colnames(newFeat) <- c('enrollment_id','freqDist')
+    x <- merge(x, newFeat, sort=F, all=T)
     
     return(x)
 }
