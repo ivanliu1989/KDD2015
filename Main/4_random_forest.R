14e590d57c9b03fd2f3a50e58284b30cff37e669
ivanliu1989 <ivan.liuyanfeng@gmail.com>
2015-05-23 21:41:33 +1000

    feature engineering complete

--- Main/4_random_forest.R
+++ Main/4_random_forest.R
@@ -17,11 +17,27 @@ levels(train_df$dropout) <- c('No','Yes')
 ### modeling ###
 fitControl <- trainControl(method = "adaptive_cv", number = 10, repeats = 5,
                            classProbs = TRUE, summaryFunction = twoClassSummary,
-                           adaptive = list(min = 10,alpha = 0.05,
+                           adaptive = list(min = 8,alpha = 0.05,
                                            method = "BT",complete = TRUE))
 set.seed(825)
 svmFit <- train(dropout ~ ., data = train_df,method = "gbm",
                 trControl = fitControl, preProc = c("center", "scale"),
-                tuneLength = 8,metric = "ROC",verbose =T)
+                tuneLength = 6,metric = "ROC",verbose =T)
 
-predict(gbmFit, newdata = val_df, type = "prob")
+pred <- predict(gbmFit, newdata = val_df, type = "prob")
+
+### Validation ###
+auc <- function(predict, target) {
+    rocr <- prediction(predict[, 2], target)
+    roc <- performance(rocr, "tpr", "fpr")
+    plot(roc, colorize = TRUE)
+    performance(rocr, "auc")@y.values
+}
+target_val = val_df$dropout
+score <- auc(pred, target_val)
+confusionMatrix(pred, target_val)
+sensitivity(pred, target_val)
+
+### Variable imp ###
+gbmImp <- varImp(gbmFit, scale = FALSE)
+plot(gbmImp, top = 20)
