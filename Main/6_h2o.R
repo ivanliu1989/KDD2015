3a87ebe8d9e2c145cc97c996408ec33fe42017d1
ivanliu1989 <ivan.liuyanfeng@gmail.com>
2015-06-14 13:36:04 +1000

    h2o 3.0

--- Main/6_h2o.R
+++ Main/6_h2o.R
@@ -12,8 +12,13 @@ train_df <- train[,-which(names(train) %in% c('course_id', 'enrollment_id', 'use
 val_df <- val[,-which(names(val) %in% c('course_id', 'enrollment_id', 'username'))]
 test_df <- test[,-which(names(test) %in% c('course_id', 'enrollment_id', 'username'))]
 train_df <- train_df[,c(1:54,56:70,55)]
+val_df <- val_df[,c(1:54,56:70,55)]
+train_df[,70] <- as.factor(train_df[,70])
+levels(train_df[,70]) <- c('No', 'Yes')
+val_df[,70] <- as.factor(val_df[,70])
+levels(val_df[,70]) <- c('No', 'Yes')
 
-h2o.shutdown(localH2O)
+# h2o.shutdown(localH2O)
 localH2O <- h2o.init(nthread=3,Xmx="12g")
 
 train.hex <- as.h2o(localH2O,train_df)
@@ -29,60 +34,108 @@ for(i in 1:20){
     # deep learning
     model <- h2o.deeplearning(x=predictors,
                               y=response,
-                              data=train.hex,
-                              classification=T,
-                              activation="RectifierWithDropout",
-                              hidden=c(512,256,128),
-                              hidden_dropout_ratio=c(0.25,0.25,0.25),
-                              input_dropout_ratio=0.15,
-                              epochs=300,
-                              l1=1e-5,
-                              l2=1e-5,
-                              rho=0.99,
-                              epsilon=1e-8,
-                              train_samples_per_iteration=2000,
-                              max_w2=10,
+                              training_frame=train.hex,
+                              overwrite_with_best_model = T,
+                              autoencoder=T,
+                              # use_all_factor_levels=T,
+                              activation = "RectifierWithDropout",
+                              hidden = c(512,256,128),
+                              epochs = 100,
+                              train_samples_per_iteration = -1,
+                              adaptive_rate = T,
+                              rho = 0.99,
+                              epsilon = 1.0e-5,
                               rate = 0.01,
-                              #rate_annealing=,
-                              #rate_decay=,
+                              rate_annealing = 1e-06,
+                              rate_decay = 1,
                               momentum_start=0.5,
-                              momentum_stable=0.99,
+                              # momentum_ramp = 1e+06,
+                              momentum_stable = 0.99,
                               nesterov_accelerated_gradient=T,
-                              loss='CrossEntropy',
+                              input_dropout_ratio = 0.5,
+                              hidden_dropout_ratios = 0.5,
+                              l1 = 1e-05,
+                              l2 = 1e-05,
+                              loss = 'CrossEntropy',
+                              classification_stop=0,
+                              quiet_mode=F,
+                              balance_classes=F,
+                              diagnostics=T,
+                              variable_importances=T,
+                              fast_mode=F,
+                              ignore_const_cols=T,
+                              single_node_mode=T,
                               shuffle_training_data=T,
+                              sparse=T,
+                              #col_major
+                              average_activation=T,
+                              # sparsity_beta = 
                               seed=8)
+    # Naive Bayes
+    model <- h2o.naiveBayes(x=predictors,
+                            y=response,
+                            training_frame=train.hex,
+                            laplace=0,#3
+                            threshold=0.001,
+                            # eps=0,
+                            compute_metrics=T)
     # random forest
     model <- h2o.randomForest(x=predictors,
                               y=response,
-                              data=train.hex,
-                              classification=T,
-                              ntree = 1500,
-                              depth = 8,
-                              mtries = 8,
-                              #sample.rate = 0.8,
+                              training_frame=train.hex,
+                              ntrees = 1500,
+                              max_depth = 8,
+                              min_rows=1,
+                              balance_classes=F,
+                              mtries = -1,
+                              sample_rate = 0.7,
+                              build_tree_one_node=F,
                               nbins = 20,
-                              importance = T,
-                              score.each.iteration = T,
-                              validation = val.hex,
-                              balance.classes =F,
-                              max.after.balance.size = 1,
-                              verbose=T,
-                              type='BigData',
-                              stat.type = 'ENTROPY', #'GINI', 'TWOING'
+                              score_each_iteration = T,
+                              balance_classes =F,
+                              # nbins = 256,
+                              binomial_double_trees =T,
+                              # max_after_balance_size = 1,
+                              # validation_frame = val.hex,
                               seed=8)
+    # glm
+    model <- h2o.glm(x=predictors,
+                     y=response,
+                     training_frame = train.hex,
+                     max_iterations = 1000,
+                     #beta_epsilon
+                     #solver = 'L_BFGS',#IRLSM
+                     #standardize = T,
+                     family = 'binomial',
+                     #link = 'logit', #log
+                     #alpha = 0.5,
+                     #prior = 1,
+                     #lambda = 0.01,
+                     #lambda_search = T,
+                     #nlambdas
+                     #lambda_min_ratio = 0.0001,
+                     #offset_column
+                     #weights_column
+                     intercept=T
+                     )
     # gbm
     model <- h2o.gbm(x=predictors,
                      y=response,
-                     data=train.hex,
+                     training_frame = train.hex,
                      distribution="bernoulli",
-                     n.trees=1000,
-                     interaction.depth=8,
-                     #n.minobsinnode=1,
-                     shrinkage=0.01,
-                     n.bins=18,
-                     balance.classes=T)
+                     ntrees=900,
+                     max_depth=8,
+                     # min_rows=10,
+                     learn_rate=0.01,
+                     nbins=128,
+                     #nbins_cats=1024,
+                     balance_classes=F,
+                     seed=8)
+    model@model$scoring_history
+    model@model$variable_importances
+    #max_after_balance_size
     
-    pred = as.data.frame(h2o.predict(model,val.hex))
+    pred = as.data.frame(predict(model,val.hex))
     score <- auc(pred[,2:3], target_val);print(score)
     write.csv(pred, file=paste0('results/valPred_h2o_deeplearning_',score,'.csv'),row.names=F, quote=F)
     
